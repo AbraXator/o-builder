8263f83ef1533e949350278f77412af95ff1c1d9
AbraXator <adammek.mack@gmail.com>
2025-07-10 20:45:42 +0200

    first page init

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import MapPage from './MapPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1 class="text-3xl text-red-500 font-bold underline">    
-      Hello world!  
-    </h1>
+    <MapPage/>
   )
 }
 
